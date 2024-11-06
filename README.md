411928c57e7d131ebcd54e130d958a009769b5d8
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-11-06 17:54:41 +0100

    review design

---
+++ README.md
@@ -0,0 +1,17 @@
+# ChatBot AI Assistant with Open AI
+
+## Get started with the Open API
+
+This repository contains an app demonstrating how to use open ai API, Speech reorganization and textToSpeech
+
+To try out the sample app you can directly import the project from Android Studio
+
+1. Go to https://platform.openai.com/docs/overview
+2. Login with your account.
+3. Create an API key.  you can use project api keys https://platform.openai.com/api-keys
+4. Check out this repository. in utils folder Constants file replace API_KEY
+5. Run the app
+
+<video width="320" height="240" controls>
+  <source src="Screen_recording_chatbotAI.mp4" type="video/mp4">
+</video>
--- app/src/main/java/com/yaropaul/chatbotaiassistant/ui/chat/ChatScreen.kt
+++ app/src/main/java/com/yaropaul/chatbotaiassistant/ui/chat/ChatScreen.kt
@@ -7,9 +7,10 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,9 +18,9 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -46,18 +47,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.yaropaul.chatbotaiassistant.R
 import com.yaropaul.chatbotaiassistant.data.model.Message
 import com.yaropaul.chatbotaiassistant.data.model.User
+import com.yaropaul.chatbotaiassistant.ui.theme.ChatBotAIAssistantTheme
 import com.yaropaul.chatbotaiassistant.utils.Event
+import kotlinx.coroutines.delay
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -68,7 +77,6 @@ fun ChatScreen(
     onClearMessages: () -> Unit,
     onToggleTTS: () -> Unit,
     isTTSEnabled: Boolean,
-    isListening: Boolean,
     isConnected: Boolean,
     errorMessage: Event<String>?,
     currentUser: User,
@@ -78,6 +86,7 @@ fun ChatScreen(
     var userInput by remember { mutableStateOf(TextFieldValue("")) }
     var showDeleteConfirmationDialog by remember { mutableStateOf(false) }
     var showNoConnectionDialog by remember { mutableStateOf(false) }
+
 
     // Managing microphone permissions
     val microphonePermission = rememberLauncherForActivityResult(
@@ -117,19 +126,28 @@ fun ChatScreen(
             Column {
                 TopAppBar(
                     colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        titleContentColor = Color.White
+                        containerColor = MaterialTheme.colorScheme.background,
+                        titleContentColor = MaterialTheme.colorScheme.onSurface,
                     ),
-                    title = { Text("ChatBot AI Assistant") },
+                    title = { Text("ChatBot AI") },
                     actions = {
-                        IconButton(onClick = { showDeleteConfirmationDialog = true }) {
+                        IconButton(
+                            onClick = { showDeleteConfirmationDialog = true },
+                            modifier = Modifier
+                                .size(48.dp),
+                        ) {
                             Icon(
                                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_clear_all),
                                 contentDescription = "Delete messages"
                             )
                         }
+                        Spacer(modifier = Modifier.width(8.dp))
                         // IconButton
-                        IconButton(onClick = onToggleTTS) {
+                        IconButton(
+                            onClick = onToggleTTS,
+                            modifier = Modifier
+                                .size(48.dp),
+                        ) {
                             Icon(
                                 imageVector = if (isTTSEnabled)
                                     ImageVector.vectorResource(id = R.drawable.ic_voice_over_on)
@@ -152,8 +170,7 @@ fun ChatScreen(
                         onSendMessage(userInput.text)
                         userInput = TextFieldValue("")
                     }
-                },
-                isListening = isListening
+                }
             )
         },
         content = { paddingValues ->
@@ -170,64 +187,87 @@ fun ChatScreen(
                 )
 
                 // Showing Snack bar for errors
-                if (showError) {
-                    LaunchedEffect(Unit) {
-                        kotlinx.coroutines.delay(2000)
-                        showError = false
-                    }
-                    Snackbar(
-                        modifier = Modifier.padding(8.dp),
-                        action = {
-                            TextButton(onClick = { showError = false }) {
-                                Text("Close")
-                            }
-                        },
-                        containerColor = Color.Red
-                    ) {
-                        Text(text = errorText, color = Color.White)
-                    }
-                }
+                ShowingSnackBar(showError, errorText)
             }
 
             // Delete confirmation dialog
-            if (showDeleteConfirmationDialog) {
-                AlertDialog(
-                    onDismissRequest = { showDeleteConfirmationDialog = false },
-                    title = { Text("Confirmation") },
-                    text = { Text("Are you sure you want to delete everything?") },
-                    confirmButton = {
-                        TextButton(onClick = {
-                            onClearMessages()
-                            showDeleteConfirmationDialog = false
-                        }) {
-                            Text("OK")
-                        }
-                    },
-                    dismissButton = {
-                        TextButton(onClick = { showDeleteConfirmationDialog = false }) {
-                            Text("Cancel")
-                        }
-                    }
-                )
-            }
+            DeleteConfirmationDialog(
+                showDeleteConfirmationDialog,
+                onClearMessages,
+                onDismiss = { showDeleteConfirmationDialog = false })
 
             // No internet connection dialog
-            if (showNoConnectionDialog) {
-                AlertDialog(
-                    onDismissRequest = { showNoConnectionDialog = false },
-                    title = { Text("No internet connection") },
-                    text = { Text("Please check your internet connection") },
-                    confirmButton = {
-                        Button(onClick = {
-                            showNoConnectionDialog = false
-                        }) {
-                            Text("OK")
-                        }
-                    }
-                )
-            }
+            ConnectionDialog(showNoConnectionDialog, onDismiss = { showNoConnectionDialog = false })
         }
     )
+}
+
+@Composable
+private fun ShowingSnackBar(showError: Boolean, errorText: String) {
+    var showError1 = showError
+    if (showError1) {
+        LaunchedEffect(Unit) {
+            delay(2000)
+            showError1 = false
+        }
+        Snackbar(
+            modifier = Modifier.padding(8.dp),
+            action = {
+                TextButton(onClick = { showError1 = false }) {
+                    Text("Close")
+                }
+            },
+            containerColor = Color.Red
+        ) {
+            Text(text = errorText, color = Color.White)
+        }
+    }
+}
+
+@Composable
+private fun ConnectionDialog(showNoConnectionDialog: Boolean, onDismiss: () -> Unit) {
+    if (showNoConnectionDialog) {
+        AlertDialog(
+            onDismissRequest = { onDismiss() },
+            title = { Text("No internet connection") },
+            text = { Text("Please check your internet connection") },
+            confirmButton = {
+                Button(onClick = {
+                    onDismiss()
+                }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    showDeleteConfirmationDialog: Boolean,
+    onClearMessages: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    if (showDeleteConfirmationDialog) {
+        AlertDialog(
+            onDismissRequest = { onDismiss() },
+            title = { Text("Confirmation") },
+            text = { Text("Are you sure you want to delete everything?") },
+            confirmButton = {
+                TextButton(onClick = {
+                    onClearMessages()
+                    onDismiss()
+                }) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { onDismiss() }) {
+                    Text("Cancel")
+                }
+            }
+        )
+    }
 }
 
 
@@ -236,42 +276,50 @@ fun BottomInputBar(
     userInput: TextFieldValue,
     onUserInputChange: (TextFieldValue) -> Unit,
     onSendMessage: () -> Unit,
-    isListening: Boolean,
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
-    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == Activity.RESULT_OK) {
-            val data = it.data
-            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-            val recognizedText = result?.get(0) ?: "No speech detected."
-            onUserInputChange(TextFieldValue(recognizedText))
-            onSendMessage()
-        } else {
-            Toast.makeText(context, "[Speech recognition failed.]", Toast.LENGTH_SHORT).show()
+    val launcher =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val data = it.data
+                val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                val recognizedText = result?.get(0) ?: "No speech detected."
+                onUserInputChange(TextFieldValue(recognizedText))
+                onSendMessage()
+            } else {
+                Toast.makeText(context, "[Speech recognition failed.]", Toast.LENGTH_SHORT).show()
+            }
         }
-    }
 
-    Row(
+    ConstraintLayout(
         modifier = modifier
-            .background(MaterialTheme.colorScheme.surface)
+            .background(MaterialTheme.colorScheme.background)
             .fillMaxWidth()
             .padding(8.dp)
             .navigationBarsPadding()
-            .imePadding(),
-        verticalAlignment = Alignment.CenterVertically
+            .imePadding()
     ) {
+        val (inputField, micButton, sendButton) = createRefs()
         OutlinedTextField(
             value = userInput,
             onValueChange = onUserInputChange,
             modifier = Modifier
-                .weight(1f)
-                .height(56.dp),
+                .constrainAs(inputField) {
+                    start.linkTo(parent.start)
+                    end.linkTo(micButton.start, margin = 8.dp)
+                    width = Dimension.fillToConstraints
+                }
+                .background(
+                    MaterialTheme.colorScheme.onPrimary,
+                    shape = MaterialTheme.shapes.medium
+                )
+                .height(52.dp),
             placeholder = { Text("Type your message") },
             colors = OutlinedTextFieldDefaults.colors(
-                focusedTextColor = Color.White
+                focusedTextColor = MaterialTheme.colorScheme.onBackground
             ),
-            shape = CircleShape,
+            shape = RoundedCornerShape(12.dp),
             singleLine = true
         )
 
@@ -279,19 +327,25 @@ fun BottomInputBar(
         IconButton(
             onClick = {
                 val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                intent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
                 intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Go on then, say something.")
                 launcher.launch(intent)
             },
             modifier = Modifier
-                .size(56.dp)
-                .padding(start = 8.dp)
+                .constrainAs(micButton) {
+                    start.linkTo(inputField.end, margin = 8.dp)
+                    end.linkTo(sendButton.start, margin = 8.dp)
+                }
+                .size(48.dp)
+                .clipToBounds()
         ) {
             Icon(
                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_mic_none),
-                contentDescription = "Button Mic",
-                tint =  MaterialTheme.colorScheme.primary
+                contentDescription = "Button Mic"
             )
         }
 
@@ -299,13 +353,15 @@ fun BottomInputBar(
         IconButton(
             onClick = onSendMessage,
             modifier = Modifier
-                .size(56.dp)
-                .padding(start = 8.dp)
+                .size(48.dp)
+                .constrainAs(sendButton) {
+                    end.linkTo(parent.end)
+                },
+            interactionSource = remember { MutableInteractionSource() }
         ) {
             Icon(
                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_send),
-                contentDescription = "Button send",
-                tint = MaterialTheme.colorScheme.primary
+                contentDescription = "Button send"
             )
         }
     }
@@ -374,7 +430,10 @@ fun MessageItem(
                 if (message.imageUrl != null) {
                     // Using Coil to load images
                     AsyncImage(
-                        model = message.imageUrl,
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(message.imageUrl)
+                            .crossfade(true)
+                            .build(),
                         contentDescription = "Image of chatbot",
                         modifier = Modifier
                             .fillMaxWidth()
@@ -395,4 +454,84 @@ fun MessageItem(
             modifier = Modifier.padding(top = 2.dp)
         )
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ChatScreenLightPreview() {
+    ChatBotAIAssistantTheme(darkTheme = false) {
+        ChatScreen(
+            messages = sampleMessagesContent(),
+            onSendMessage = {},
+            onClearMessages = {},
+            onToggleTTS = {},
+            isTTSEnabled = false,
+            isConnected = true,
+            errorMessage = null,
+            currentUser = User("1", "User", ""),
+            chatGptUser = User("2", "ChatGPT", "")
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChatScreenDarkPreview() {
+    ChatBotAIAssistantTheme(darkTheme = true) {
+        ChatScreen(
+            messages = sampleMessagesContent(),
+            onSendMessage = {},
+            onClearMessages = {},
+            onToggleTTS = {},
+            isTTSEnabled = true,
+            isConnected = false,
+            errorMessage = null,
+            currentUser = User("1", "User", ""),
+            chatGptUser = User("2", "ChatGPT", "")
+        )
+    }
+}
+
+private fun sampleMessagesContent(): List<Message> {
+    val currentUser = User("1", "User", "")
+    val chatGptUser = User("2", "ChatGPT", "")
+    return listOf(
+        Message(
+            id = "1",
+            text = "Bonjour, comment ça va ?",
+            userId = currentUser.id,
+            userName = currentUser.name,
+            userAvatar = currentUser.avatar,
+            date = Date(),
+            imageUrl = null
+        ),
+        Message(
+            id = "2",
+            text = "Je vais bien, merci ! Comment puis-je vous aider aujourd'hui ?",
+            userId = chatGptUser.id,
+            userName = chatGptUser.name,
+            userAvatar = chatGptUser.avatar,
+            date = Date(),
+            imageUrl = null
+        ),
+        Message(
+            id = "3",
+            text = "Pouvez-vous me donner la météo pour aujourd'hui ?",
+            userId = currentUser.id,
+            userName = currentUser.name,
+            userAvatar = currentUser.avatar,
+            date = Date(),
+            imageUrl = null
+        ),
+        Message(
+            id = "4",
+            text = "Bien sûr ! Aujourd'hui, il fera ensoleillé avec une température maximale de 25 degrés.",
+            userId = chatGptUser.id,
+            userName = chatGptUser.name,
+            userAvatar = chatGptUser.avatar,
+            date = Date(),
+            imageUrl = null
+        )
+    )
 }
